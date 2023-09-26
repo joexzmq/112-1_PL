240b0ceb7f3c937ffec499459da1606ce2d80ba1
莊孟喬 <a7833322010@gmail.com>
2023-09-26 08:56:54 +0800

    a

--- 0921.py
+++ 0921.py
@@ -5,8 +5,8 @@ import matplotlib
 import seaborn as sns
 
 file_names = ['misterdonut.csv', 'starbucks.csv']  # 將檔案名稱加入清單中
-dfs = [pd.read_csv(file)['country'] for file in file_names]  # 使用清單推導式讀取country欄位資料
+df1 = [pd.read_csv(file)['country'] for file in file_names]  # 使用清單推導式讀取country欄位資料
 
 # 將國家名稱轉換為 set 資料型態
-set_1 = set(dfs[0])
-set_2 = set(dfs[1])
+set1_1 = set(dfs[0])
+set1_2 = set(dfs[1])
