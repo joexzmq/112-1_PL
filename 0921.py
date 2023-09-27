b31ae047262fec3f8100de69ccdf20f5c47b5522
莊孟喬 <a7833322010@gmail.com>
2023-09-27 14:08:55 +0800

    S

--- 0921.py
+++ 0921.py
@@ -4,9 +4,25 @@ import csv
 import matplotlib
 import seaborn as sns
 
-file_names = ['misterdonut.csv', 'starbucks.csv']  # 將檔案名稱加入清單中
-df1 = [pd.read_csv(file)['country'] for file in file_names]  # 使用清單推導式讀取country欄位資料
+file_names = ['nba0001.csv', 'nba2223.csv']  # 將檔案名稱加入清單中
+df1 = [pd.read_csv(file)['team'] for file in file_names]  # 使用清單推導式讀取team欄位資料
+df2 = [pd.read_csv(file)['rate'] for file in file_names]  # 使用清單推導式讀取rate欄位資料
+df0 = [pd.read_csv(file) for file in file_names]  # 使用清單推導式讀取team欄位資料
 
-# 將國家名稱轉換為 set 資料型態
-set1_1 = set(dfs[0])
-set1_2 = set(dfs[1])
+# 將欄位資料轉換為 set 資料型態
+set1_1 = set(df1[0])
+set1_2 = set(df1[1])
+set2_1 = set(df2[0])
+set2_2 = set(df2[1])
+
+team = np.arange(30)
+rate = np.arange(58)
+
+years = np.linspace(1960, 2017, 58)
+numbers = np.arange(58)
+
+trace = sns.scatterplot(
+    x = years,
+    y = numbers)
+
+print(df0)
